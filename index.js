48e3c71f8c0d4263e720f9ee16351aa9f302be62
Jason S. Jones <jsjones96@gmail.com>
2015-02-06 05:51:03 -0800

    add more to the skeleton

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+/**
+ * @fileOverview Implementation of a set data structure
+ * @author Jason S. Jones
+ * @version 0.1.0
+ * @license MIT
+ */
 (function () {
     'use strict';
+
+    /**
+     * Creates a new set instance and initializes the underlying data
+     * structure
+     *
+     * @constructor
+     */
+    function Set() {
+    }
+
+    Set.prototype = {
+
+    };
+
+    module.exports = Set;
 
 })();
