5da816bb75585873f19fdec81e2cba1546f64196
Jason S. Jones <jsjones96@gmail.com>
2015-02-10 05:44:24 -0800

    implement basic methods

--- index.js
+++ index.js
@@ -14,12 +14,48 @@
      * @constructor
      */
     function Set() {
+        this._items = [];
     }
 
     Set.prototype = {
 
+        add: function (value) {
+            if (!this.has(value)) {
+                this._items.push(value);
+                return true;
+            }
+            return false;
+        },
+
+        remove: function (value) {
+            var idx = this._items.indexOf(value);
+            return this._items.splice(idx, 1);
+        },
+
+        has: function (value) {
+            return this._items.indexOf(value) > -1;
+        },
+
+        clear: function () {
+            this._items = [];
+        },
+
+        size: function () {
+            return this._items.length;
+        },
+
+        isEmpty: function () {
+            return this.size() === 0;
+        },
+
+        values: function () {
+            return this._items;
+        }
     };
 
+    /*
+     * Expose Set
+     */
     module.exports = Set;
 
 })();
