de801bf722ac3a1a8b4bd48dee251f51c371567f
Jason Jones <jsjones96@gmail.com>
2015-02-11 00:09:39 +0000

    add jsdoc comments/documentation

--- index.js
+++ index.js
@@ -17,8 +17,22 @@
         this._items = [];
     }
 
+    /* Functions attached to the Set prototype.  All set instances will share
+     * these methods, meaning there will NOT be copies made for each
+     * instance.  This will be a huge memory savings since there may be several
+     * different set instances.
+     */
     Set.prototype = {
 
+        /**
+         * Adds an item to the set.  If the set already contains the item,
+         * it is not added.
+         *
+         * @param {object|string|number} value the data of the item to add to
+         *        the set
+         * @returns {boolean} true if the item is added to the set; false
+         *          otherwise
+         */
         add: function (value) {
             if (!this.has(value)) {
                 this._items.push(value);
@@ -27,6 +41,14 @@
             return false;
         },
 
+        /**
+         * Removes an item from the set.
+         *
+         * @param {object|string|number} value the data of the item to remove
+         *        from the set
+         * @returns {object|string|number} the item that was removed from the
+         *          set.  If the item is not in the set, returns null
+         */
         remove: function (value) {
             var idx = this._items.indexOf(value);
             if (idx === -1) {
@@ -36,22 +58,47 @@
             }
         },
 
+        /**
+         * Determines of the set contains, or has, the value
+         *
+         * @param {object|string|number} value the value of the item to find
+         *        in the set
+         * @returns {boolean} true if the set has the value; false otherwise
+         */
         has: function (value) {
             return this._items.indexOf(value) > -1;
         },
 
+        /**
+         * Clears all the items from the set
+         */
         clear: function () {
             this._items = [];
         },
 
+        /**
+         * Returns the size, or number of items in the set
+         *
+         * @returns {number} the number of items in the set
+         */
         size: function () {
             return this._items.length;
         },
 
+        /**
+         * Determines if the set is empty
+         *
+         * @returns {boolean} true if the set is empty, false otherwise
+         */
         isEmpty: function () {
             return this.size() === 0;
         },
 
+        /**
+         * Returns an array containing all the items in the set
+         *
+         * @returns {object} array of all the items in the set
+         */
         values: function () {
             return this._items;
         }
